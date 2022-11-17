c7cc3ee47a1800f8a71c276322ad873f8fe1dcf5
priyasjsu <priya.khandelwal@sjsu.edu>
2022-11-17 12:49:39 -0800

    Initial check-in

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import TableauReport from "tableau-react";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1> Data 1 </h1>
+        <TableauReport url="https://10az.online.tableau.com/t/priya228/views/tableau-Book1_test/Test_Dashboard1" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
