208833348a81ee7bf66cac4d371fcca6c41aae1d
priyasjsu <priya.khandelwal@sjsu.edu>
2022-11-30 14:45:30 -0800

    UI change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import '@aws-amplify/ui-react/styles.css'
+import '@aws-amplify/ui-react/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+// import MenuItem from 'react-bootstrap/MenuItem';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -18,14 +30,33 @@ root.render(
     <Authenticator>
       {({ signOut, user }) => (
         <div>
-          {/* {console.log(user)} */}
-          {/* <h1></h1> */}
-          <IconContext.Provider value={{ size : "2em" }}>
-          <div className = "pericon"> <BsPersonCircle/></div>
-          </IconContext.Provider>
-          <button onClick={signOut}>Sign out</button>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container fluid className="mt-3">
+            <Navbar.Brand href="javascript:void(0)">Dashboard</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link target="_blank" href="http://ec2-54-241-114-164.us-west-1.compute.amazonaws.com:8080/">Customer Value Prediction</Nav.Link>
+              </Nav>
+              <Nav>
+                <NavDropdown title={<span><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+</svg></span>} id="basic-nav-dropdown" align="end">
+                  <NavDropdown.Item href="javascript:void(0)">
+                    <small>Logged in as:</small><br />
+                    <strong>Priya Khandelwal</strong>
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="javascript:void(0)" onClick={signOut}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Container>
           <App />
-          </div>
+        </Container>
+        </div>
       )}
     </Authenticator>
   </React.StrictMode>
